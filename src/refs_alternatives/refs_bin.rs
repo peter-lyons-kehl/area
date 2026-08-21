943dd5f31e49ec74be7cc83fd80034fe134d004c
Peter Lyons Kehl <peter.kehl@gmail.com>
2026-08-21 16:52:07 -0700

    WIP

--- src/refs_alternatives/refs_bin.rs
+++ src/refs_alternatives/refs_bin.rs
@@ -25,20 +25,20 @@ impl<'_ta, T, _AWI: AddrWidthIndicator> Deref for RefBin<'_ta, T, _AWI> {
 
 pub trait ResolvableKids {
     type TO;
-    fn resolve_kids<AWI: AddrWidthIndicator>(&self, area: &Area<AWI>) -> &Self::TO;
+    fn resolve<AWI: AddrWidthIndicator>(&self, area: &Area<AWI>) -> &Self::TO;
 }
 
 impl<'_ta, T, _AWI: AddrWidthIndicator> RefBin<'_ta, T, _AWI>
 where
     T: ResolvableKids,
 {
-    /// An alternative to [ResolvableKids::resolve_kids], in case `T` type itself, or its
+    /// An alternative to [ResolvableKids::resolve], in case `T` type itself, or its
     /// another trait, also has a `resolve` method (which would then conflict with
-    /// [ResolvableKids::resolve_kids] if trait [ResolvableKids] were imported).
-    pub fn resolve_kids<'ta, AWI: AddrWidthIndicator>(
+    /// [ResolvableKids::resolve] if trait [ResolvableKids] were imported).
+    pub fn resolve<'ta, AWI: AddrWidthIndicator>(
         this: &'ta Self,
         area: &'ta Area<AWI>,
     ) -> &'ta <T as ResolvableKids>::TO {
-        ResolvableKids::resolve_kids(this.ref_t, area)
+        ResolvableKids::resolve(this.ref_t, area)
     }
 }
